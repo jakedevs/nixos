9fefc26e3cb4869c9fecd770c1bf339d82eccfac
Jake Levi <jakedevquestions@gmail.com>
2024-03-11 07:06:59 -0400

    update nouveau

--- modules/nixos/nouveau.nix
+++ modules/nixos/nouveau.nix
@@ -2,4 +2,19 @@
   boot.initrd.kernelModules = [ "nouveau" ];
   services.xserver.videoDrivers = [ "nouveau" ];
   chaotic.mesa-git.enable = true;
+
+  environment.systemPackages = with pkgs;
+    [
+      libdrm_git
+      libdrm32_git
+      libva
+      libva-utils
+      mesa_git
+      mesa32_git
+      mesa-demos
+      vaapiVdpau
+      vulkan-tools
+      egl-wayland
+      libglvnd
+    ];
 }
