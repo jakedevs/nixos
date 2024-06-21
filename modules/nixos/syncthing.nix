ba9423defee84d49e9c262fc61c4af4c5c2f8c94
jakedevs <main@jakedevs.net>
2024-06-20 21:02:56 -0400

    add desktop "jake"

--- modules/nixos/syncthing.nix
+++ modules/nixos/syncthing.nix
@@ -17,15 +17,18 @@
       overrideDevices = true;
       overrideFolders = true;
       settings = {
-        # devices = {
-        #   "SM-G991U" = {
-        #     id = "4XQYBAU-RVG24HB-2FMFRXC-KISKNWJ-CLRFPKV-HLPJM3C-BVTV7VU-7JWBSA5";
-        #   };
-        # };
+        devices = {
+          "SM-G991U".id = "4XQYBAU-RVG24HB-2FMFRXC-KISKNWJ-CLRFPKV-HLPJM3C-BVTV7VU-7JWBSA5";
+          "jake".id = "CA4NFAX-QSQ5KRW-TNS3YMZ-NEWXXXZ-KFU4OLX-HFU4K53-T4NZM4N-DGHAIAY";
+
+        };
         folders = {
           "Sync" = {
             path = "/home/${username}/Sync";
-            # devices = [ "SM-G991U" ];
+            devices = [
+              "SM-G991U"
+              "jake"
+            ];
           };
         };
         gui = {
